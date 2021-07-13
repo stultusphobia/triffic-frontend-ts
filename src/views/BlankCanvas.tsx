a13efe47a19f7e0678c557ee20afc938ac1a8f47
Daniel Liu <daniel.liu1215@gmail.com>
2021-07-13 08:41:45 +0800

    add blank view for ui design

---
+++ src/views/BlankCanvas.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BlankCanvas = () => {
+
+  return (
+    <div />
+  )
+}
+
+export default BlankCanvas
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,9 +1,11 @@
 import Home from './Home'
 import About from './About'
 import AttractionSelector from './AttractionSelector'
+import BlankCanvas from './BlankCanvas'
 
 export default [
   { component: Home, text: 'Home', to: '/' , exact: true },
   { component: About, text: 'About', to: '/about', exact: false },
   { component: AttractionSelector, text: 'Attraction Selector', to: '/attraction-selector', exact: false },
+  { component: BlankCanvas, text: 'Blank-Canvas', to: '/blank-canvas', exact: false },
 ]
